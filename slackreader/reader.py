25844ef67620e1e77f21684d9a23e57f832ef97d
Benjamin Green <bpgreen10@gmail.com>
2018-11-28 16:12:49 +0000

    Add get_channels method.

--- slackreader/reader.py
+++ slackreader/reader.py
@@ -1,5 +1,7 @@
 import requests
 
+SLACK_URL = 'https://slack.com/api/'
+
 
 class SlackReader:
 
@@ -8,13 +10,26 @@ class SlackReader:
 
     def get_users(self, active_only=True):
 
-        url = "https://slack.com/api/users.list?token={}&pretty=1"
-        users_response = requests.get(url.format(self.api_key))
-        users = users_response.json()
-        if not users['ok']:
+        url = SLACK_URL + "users.list?token={}&pretty=1"
+        response = requests.get(url.format(self.api_key))
+        response = response.json()
+        if not response['ok']:
             raise ValueError("Problem connecting.")
         user_list = []
-        for user in users['members']:
+        for user in response['members']:
             if not user['deleted'] or not active_only:
                 user_list.append(user['id'])
         self.users = user_list
+
+    def get_channels(self):
+
+        url = SLACK_URL + "channels.list?token={}&pretty=1"
+        response = requests.get(url.format(self.api_key))
+        response = response.json()
+        if not response['ok']:
+            raise ValueError("Problem connecting.")
+        channel_list = []
+        for channel in response['channels']:
+            if channel['is_channel']:
+                channel_list.append(channel['id'])
+        self.channels = channel_list
