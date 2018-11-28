9e9ebcd4e0f739988d2f6fc1967471816c778b83
Benjamin Green <bpgreen@Benjamins-MacBook.local>
2018-11-28 15:47:05 +0000

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+      name='slackreader',
+      version='0.0.1',
+      description='A slack reader',
+      url='https://github.com/benpgreen/slack-reader',
+      author='Ben Green',
+      license='Ben Green',
+      packages=find_packages(),
+      zip_safe=False
+      )
