519a50a971e0f9c161036cb18c0c0501c5d109c5
Benjamin Green <bpgreen10@gmail.com>
2018-11-29 11:11:21 +0000

    Update connection error, and add api_key as an argument to the Channel and User classes.

--- slackreader/reader.py
+++ slackreader/reader.py
@@ -15,11 +15,11 @@ class SlackReader:
         response = requests.get(url)
         response = response.json()
         if not response['ok']:
-            raise ValueError("Problem connecting.")
+            raise RuntimeError("Problem connecting to slack.")
         user_list = []
         for user in response['members']:
             if not user['deleted'] or not active_only:
-                user_list.append(User(user))
+                user_list.append(User(self.api_key, user))
         self.users = user_list
 
     def get_channels(self):
@@ -28,9 +28,9 @@ class SlackReader:
         response = requests.get(url.format(self.api_key))
         response = response.json()
         if not response['ok']:
-            raise ValueError("Problem connecting.")
+            raise RuntimeError("Problem connecting to slack.")
         channel_list = []
         for channel in response['channels']:
             if channel['is_channel']:
-                channel_list.append(Channel(channel))
+                channel_list.append(Channel(self.api_key, channel))
         self.channels = channel_list
